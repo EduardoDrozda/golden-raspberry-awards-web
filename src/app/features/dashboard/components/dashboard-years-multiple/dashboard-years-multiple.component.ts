49898324104e1e8982e537ba87ad2eec5227818e
Eduardo Drozda <Edu_Drozda@hotmail.com>
2025-06-17 16:48:06 -0300

    feat: create dashboard-top-studios-with-winners

--- src/app/features/dashboard/components/dashboard-years-multiple/dashboard-years-multiple.component.ts
+++ src/app/features/dashboard/components/dashboard-years-multiple/dashboard-years-multiple.component.ts
@@ -1,14 +1,14 @@
 import { Component, input } from '@angular/core';
 import { WinnersByYearModel } from '@features/dashboard/models';
+import { TableComponent } from '@shared/components';
 
 @Component({
   selector: 'app-dashboard-years-multiple-list',
-  imports: [],
+  imports: [TableComponent],
   templateUrl: './dashboard-years-multiple.component.html',
   styleUrl: './dashboard-years-multiple.component.scss'
 })
 export class DashboardYearsMultipleComponent {
   winnersByYear = input.required<WinnersByYearModel | null>();
   isLoading = input<boolean>(false);
-
 }
