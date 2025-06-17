f432ada59c6225f02611620bc831ac15022048c4
Eduardo Drozda <Edu_Drozda@hotmail.com>
2025-06-17 16:05:14 -0300

    feat: create layout sidebar arquitecture

--- src/app/core/components/layout/layout.component.ts
+++ src/app/core/components/layout/layout.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { SidebarOptionsModel } from './models';
+import { RoutesEnum } from '@core/enums';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './layout.component.html',
-  styleUrl: './layout.component.scss'
+  styleUrl: './layout.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LayoutComponent {
-
+  sidebarOptions: SidebarOptionsModel[] = [
+    {
+      title: 'Dashboard',
+      route: RoutesEnum.DASHBOARD,
+      icon: 'fa-solid fa-tachometer-alt'
+    },
+    {
+      title: 'Movies',
+      route: RoutesEnum.MOVIES,
+      icon: 'fa-solid fa-film'
+    }
+  ];
 }
---
+++ src/app/core/components/layout/models/sidebar-options.model.ts
@@ -0,0 +1,5 @@
+export type SidebarOptionsModel = {
+  title: string;
+  route: string;
+  icon?: string; // Optional property for an icon
+}
